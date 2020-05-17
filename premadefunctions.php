f2cbcab1cc426ef272648fc6036b6e1a0c7eff80
John Bostick <jbost0114@gmail.com>
2020-05-17 17:19:51 -0400

    Update premadefunctions.php

--- premadefunctions.php
+++ premadefunctions.php
@@ -103,8 +103,6 @@ function styledHTML($title, $content, $style) {
 	</html>
 	";
 }
-
-// Credit to dzone.com for the below functions:
 function source_code($site) {
 	$lines = file($site);
 	foreach ($lines as $line_num => $line) { 
@@ -129,3 +127,93 @@ function memory_usage() {
 	echo "Final: ".memory_get_usage()." bytes <br />";
 	echo "Peak: ".memory_get_peak_usage()." bytes <br />";
 }
+function discordOauth($client_id, $client_secret, $redirect_uri, $permissions) {
+	session_start();
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	ini_set('max_execution_time', 300); //300 seconds = 5 minutes. In case if your CURL is slow and is loading too much (Can be IPv6 problem)
+	error_reporting(E_ALL);
+
+	define('OAUTH2_CLIENT_ID', $client_id);
+	define('OAUTH2_CLIENT_SECRET', $client_secret);
+
+	$authorizeURL = 'https://discordapp.com/api/oauth2/authorize?client_id=656990364762243103&redirect_uri='.$redirect_uri.'&response_type=code&scope='.$permissions.'';
+	$tokenURL = 'https://discordapp.com/api/oauth2/token';
+	$apiURLBase = 'https://discordapp.com/api/users/@me';
+	if(isset($_GET['action'])) {
+	if($_GET['action'] == 'login') {
+
+		$params = array(
+		  'client_id' => OAUTH2_CLIENT_ID,
+		  'redirect_uri' => $redirect_uri,
+		  'response_type' => 'code',
+		  'scope' => $permissions
+		);
+	  
+		// Redirect the user to Discord's authorization page
+		header('Location: https://discordapp.com/api/oauth2/authorize' . '?' . http_build_query($params));
+		die();
+	  }
+	}
+	  
+	  
+	  // When Discord redirects the user back here, there will be a "code" and "state" parameter in the query string
+	  if(isset($_GET['code'])) {
+	  
+		// Exchange the auth code for a token
+		$token = apiRequest($tokenURL, array(
+		  "grant_type" => "authorization_code",
+		  'client_id' => OAUTH2_CLIENT_ID,
+		  'client_secret' => OAUTH2_CLIENT_SECRET,
+		  'redirect_uri' => $redirect_uri,
+		  'code' => get('code')
+		));
+	  
+	  
+		$logout_token = $token->access_token;
+		$_SESSION['access_token'] = $token->access_token;
+	  
+	  
+		header('Location: ' . $_SERVER['PHP_SELF']);
+	  }
+	  if (isset($_GET['error'])) {
+		return "User denied the request.";
+	  } else {
+	  if(session('access_token')) {
+		
+		$user = apiRequest($apiURLBase);
+	  
+		return $user;
+	  
+	  }}
+	  
+}
+function apiRequest($url, $post=FALSE, $headers=array()) {
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  
+	$response = curl_exec($ch);
+  
+  
+	if($post)
+	  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+  
+	$headers[] = 'Accept: application/json';
+  
+	if(session('access_token'))
+	  $headers[] = 'Authorization: Bearer ' . session('access_token');
+  
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  
+	$response = curl_exec($ch);
+	return json_decode($response);
+  }
+  
+  function get($key, $default=NULL) {
+	return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
+  }
+  
+  function session($key, $default=NULL) {
+	return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
+  }
